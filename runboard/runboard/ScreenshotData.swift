f118f4c073024ef4ba033bef7652e4a1f11b6c9d
Paul Goldschmidt <mail@paul-goldschmidt.de>
2026-04-07 16:26:03 +0200

    update data

--- runboard/runboard/ScreenshotData.swift
+++ runboard/runboard/ScreenshotData.swift
@@ -21,27 +21,27 @@ enum ScreenshotData {
             BoardMember(
                 id: "RUN42X_paul", displayName: "Paul", vo2Max: 52.1, weeklyKilometers: 47.2,
                 lastUpdated: .now, isCurrentUser: true,
-                statsHistory: history([(12.5, 48.2), (18.3, 49.0), (25.1, 50.1), (32.7, 50.8), (40.0, 51.5), (47.2, 52.1)])
+                statsHistory: history([(15.0, 47.0), (22.0, 48.5), (25.5, 49.2), (30.0, 50.5), (38.5, 51.8), (47.2, 52.1)])
             ),
             BoardMember(
                 id: "RUN42X_max", displayName: "Max", vo2Max: 48.3, weeklyKilometers: 38.5,
                 lastUpdated: .now,
-                statsHistory: history([(20.0, 46.5), (25.5, 47.0), (30.2, 47.5), (28.1, 47.2), (35.0, 48.0), (38.5, 48.3)])
+                statsHistory: history([(28.0, 48.8), (32.0, 48.5), (35.5, 48.0), (36.0, 47.8), (37.2, 48.1), (38.5, 48.3)])
             ),
             BoardMember(
                 id: "RUN42X_sophie", displayName: "Sophie", vo2Max: 45.7, weeklyKilometers: 31.8,
                 lastUpdated: .now,
-                statsHistory: history([(15.0, 43.0), (20.3, 43.8), (22.8, 44.2), (25.5, 44.8), (28.0, 45.2), (31.8, 45.7)])
+                statsHistory: history([(18.0, 42.0), (24.5, 43.5), (28.0, 44.8), (22.0, 43.5), (29.5, 45.0), (31.8, 45.7)])
             ),
             BoardMember(
                 id: "RUN42X_lucas", displayName: "Lucas", vo2Max: 41.2, weeklyKilometers: 24.1,
                 lastUpdated: .now,
-                statsHistory: history([(10.5, 39.5), (15.2, 40.0), (18.0, 40.5), (20.5, 40.8), (22.0, 41.0), (24.1, 41.2)])
+                statsHistory: history([(12.0, 39.0), (14.5, 39.8), (17.0, 40.2), (19.5, 40.5), (21.8, 40.9), (24.1, 41.2)])
             ),
             BoardMember(
                 id: "RUN42X_emma", displayName: "Emma", vo2Max: nil, weeklyKilometers: 18.6,
                 lastUpdated: .now,
-                statsHistory: history([(8.0, nil), (10.5, nil), (12.8, nil), (14.2, nil), (16.5, nil), (18.6, nil)])
+                statsHistory: history([(14.0, nil), (16.5, nil), (15.0, nil), (14.8, nil), (16.0, nil), (18.6, nil)])
             ),
         ]
 
